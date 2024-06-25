441ee8c58e691db107b992ee1857bff151e9661e
alannofrialdi <alannofrialdi098@gmail.com>
2024-06-25 19:42:24 +0700

    asdk

---
+++ index.js
@@ -0,0 +1,34 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+const router = jsonServer.router("db.json");
+
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
+
+server.use(middlewares);
+// Add this before server.use(router)
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//     "/blog/:resource/:id/show": "/:resource/:id",
+//   }),
+// );
+server.use(router);
+server.listen(port, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
